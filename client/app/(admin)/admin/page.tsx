fe302b3d430620c1cda3a65ccf73182c4f3e1241
GatlinThompson <dreigoose@gmail.com>
2026-01-11 13:51:51 -0700

    fix: update import path for AdminLoginForm in Admin page

--- client/app/(admin)/admin/page.tsx
+++ client/app/(admin)/admin/page.tsx
@@ -1,5 +1,5 @@
 import Main from "@/components/layout/Main";
-import AdminLoginForm from "./_components/AdminLoginForm";
+import AdminLoginForm from "@/components/features/auth/AdminLoginForm";
 import GlassContainer from "@/components/ui/glass_card/GlassContainer";
 
 export default function Admin() {
