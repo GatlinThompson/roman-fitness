294ac4279e87b7430ae7115868ca8374a6fe467c
GatlinThompson <dreigoose@gmail.com>
2026-06-16 20:46:03 -0600

    added simple demo login

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Main from "@/components/layout/Main";
 import Workout from "@/components/features/workout/Workout";
 import WorkoutHeader from "@/components/features/workout/WorkoutHeader";
 import GlassContainer from "@/components/ui/glass_card/GlassContainer";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +11,12 @@ export default function Home() {
         <WorkoutHeader className="mb-6" location="/admin" />
         <Workout />
       </GlassContainer>
+      <Link
+        href="/admin"
+        className="fixed bottom-4 right-4 px-4 py-2 text-sm font-semibold font-montserrat text-white hover:text-white bg-red-orange rounded-lg transition-colors"
+      >
+        Demo Login
+      </Link>
     </Main>
   );
 }
